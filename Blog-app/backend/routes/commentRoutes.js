7e23a69686a301bfae571861a5af3a18f10c14a9
aspire95 <your_email_associated_with_aspire95>
2026-02-18 14:14:35 +0530

    new comment

--- Blog-app/backend/routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/commentController');
-const authenticateToken = require('../middleware/authMiddleware');
-
-router.post('/:postId', authenticateToken, commentController.addComment);
-router.get('/:postId', commentController.getComments);
-
-module.exports = router;
